56719c7a7a81db4041a4b98e693651574de8f928
Christopher Wellons <wellons@nullprogram.com>
2019-06-30 21:04:47 -0400

    Add Unlicense notice to the top of the package

--- rng.go
+++ rng.go
@@ -1,3 +1,5 @@
+// This is free and unencumbered software released into the public domain.
+
 // Package rng provides several more efficient PRNGs sources for use
 // with math/rand.Rand. Each PRNG implements the math/rand.Source64
 // interface.
